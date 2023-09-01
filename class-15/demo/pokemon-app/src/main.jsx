8900070aecdb418c42d1ebbb0e54597a9339661c
JacobKnaack <jacobknck@gmail.com>
2023-09-01 12:11:49 -0700

    adds environment variables

--- class-15/demo/pokemon-app/src/main.jsx
+++ class-15/demo/pokemon-app/src/main.jsx
@@ -4,11 +4,18 @@ import App from './App.jsx'
 import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css'
 
+/**
+ * import your domain and clientID from your environment
+ * let AUTH_DOMAIN = import.meta.env.VITE_AUTH_DOMAIN
+ * let AUTH_CLIENT_ID = import.meta.env.VITE_AUTH_CLIENT_ID
+ */
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Auth0Provider
-      domain="jacobknaack.auth0.com"
-      clientId="sodU4AtfDd4gKUiYW2I82eC787mOo1LW"
+      domain=AUTH_DOMAIN
+      clientId=AUTH_CLIENT_ID
       authorizationParams={{
         redirect_uri: window.location.origin,
       }}

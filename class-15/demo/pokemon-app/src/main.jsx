fc15ae5620e105d79fbd5ef30b54c9631712d853
JacobKnaack <jacobknck@gmail.com>
2023-09-01 14:26:50 -0700

    refactors environment configuration

--- class-15/demo/pokemon-app/src/main.jsx
+++ class-15/demo/pokemon-app/src/main.jsx
@@ -4,18 +4,17 @@ import App from './App.jsx'
 import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css'
 
-/**
- * import your domain and clientID from your environment
- * let AUTH_DOMAIN = import.meta.env.VITE_AUTH_DOMAIN
- * let AUTH_CLIENT_ID = import.meta.env.VITE_AUTH_CLIENT_ID
- */
+
+// import your domain and clientID from your environment
+let AUTH_CLIENT_ID = import.meta.env.VITE_AUTH_CLIENT_ID;
+let AUTH_DOMAIN = import.meta.env.VITE_AUTH_DOMAIN;
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Auth0Provider
-      domain=AUTH_DOMAIN
-      clientId=AUTH_CLIENT_ID
+      domain={AUTH_DOMAIN}
+      clientId={AUTH_CLIENT_ID}
       authorizationParams={{
         redirect_uri: window.location.origin,
       }}
